c47e787bac855024dd0dace1fd0b7e16b682714d
Tyler Trommlitz <ttrommlitz6@gmail.com>
2023-06-15 10:29:44 -0600

    feat: add images

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,17 @@
 
 
 const Body = () => {
-  return <h1>Hi there! I'm Tyler</h1>
+  return (
+    <div className="introduction">
+      <div className="intro-section-1">
+        <h1>Hi there! I'm Tyler!</h1>
+        <img className="image-1" src="male.jpeg" alt="computer" />
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
 }
 
 export default Body
