5f8edfa70cb201764cb0b117644ad5c97c4975d2
ttrommlitz <ttromm6@byu.edu>
2023-07-18 13:07:43 -0600

    feat: move components/css

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import logo from '../react.svg';
+
+const getLinks = () => {
+  return [
+    {
+      title: 'About',
+      source: ''
+    },
+    {
+      title: 'Skills',
+      source: ''
+    },
+    {
+      title: 'Experience and Projects',
+      source: ''
+    },
+    {
+      title: 'Download Resume',
+      source: 'Resume.pdf',
+      download: 'resume'
+    }
+  ]
+}
+
+const Header  = () => {
+  return (
+  <header className="App-header">
+    <div className="Img-container">
+      <p className="Built-with">Built with</p>
+      <img src={logo} className="App-logo" alt="logo" />
+    </div>
+    <div className="Links-container">
+    { getLinks().map(link => {
+      return <a className="App-link" href={link.source} download={link.download}>{link.title}</a>
+    }) }
+    </div>
+  </header>
+  )
+}
+export default Header
